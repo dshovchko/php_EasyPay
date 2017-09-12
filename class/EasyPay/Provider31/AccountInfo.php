708e210d884b1622728c5ec13633351dc39c10f3
dshovchko <d.shovchko@gmail.com>
2017-09-12 15:10:11 +0300

    add class AccountInfo for returning an account check result

---
+++ class/EasyPay/Provider31/AccountInfo.php
@@ -0,0 +1,30 @@
+<?php
+
+class EasyPay_Provider31_AccountInfo implements Iterator
+{
+        private $_accountinfo;
+        
+        public function __construct($accountinfo) {
+                $this->_accountinfo = $accountinfo;
+        }
+        
+        public function rewind() {
+                reset($this->_accountinfo);
+        }
+        
+        public function current() {
+                return current($this->_accountinfo);
+        }
+        
+        public function key() {
+                return key($this->_accountinfo);
+        }
+        
+        public function next() {
+                next($this->_accountinfo);
+        }
+        
+        public function valid() {
+                return key($this->_accountinfo) !== null;
+        }
+}
