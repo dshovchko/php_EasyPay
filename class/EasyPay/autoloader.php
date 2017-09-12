083255c13a9de8f2e9d5e2ffe830ff883983928a
dshovchko <d.shovchko@gmail.com>
2017-09-12 15:34:46 +0300

    add autoloader function

---
+++ class/EasyPay/autoloader.php
@@ -0,0 +1,21 @@
+<?php
+
+function __autoload_EasyPay($class_name) 
+{
+        if (substr($class_name, 0, 8) != 'EasyPay_')
+        {
+                return FALSE;
+        }
+        $filename = str_replace('_', DIRECTORY_SEPARATOR, substr($class_name, 8));
+
+        $file = dirname(__FILE__).'/'.$filename.'.php';
+
+        if ( ! file_exists($file))
+        {
+                return FALSE;
+        }
+        require $file;
+}
+
+spl_autoload_extensions('.php');
+spl_autoload_register('__autoload_EasyPay');
