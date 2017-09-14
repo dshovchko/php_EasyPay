33eec81fbf5a157cf8572fd848dcb9d552773398
dshovchko <d.shovchko@gmail.com>
2017-09-14 16:00:43 +0300

    some improvements in using namespaces

--- class/EasyPay/Provider31/Response/Check.php
+++ class/EasyPay/Provider31/Response/Check.php
@@ -1,28 +1,29 @@
 <?php
 
-	namespace EasyPay\Provider31\Response;
-	
-	use EasyPay\Provider31 as Provider31;
-	
-	final class Check extends Provider31\Response
-	{
-		function __construct(Provider31\AccountInfo $accountinfo) {
-			parent::__construct();
-			
-			$this->setElementValue('StatusCode', 0);
-			$this->setElementValue('StatusDetail', 'checked');
-			
-			$this->create_AccountInfo($accountinfo);
-		}
+namespace EasyPay\Provider31\Response;
+
+use \EasyPay\Provider31\Response as Response;
+use \EasyPay\Provider31\AccountInfo as AccountInfo;
+
+final class Check extends Response
+{
+	function __construct(AccountInfo $accountinfo) {
+		parent::__construct();
 		
-		public function create_AccountInfo($accountinfo)
+		$this->setElementValue('StatusCode', 0);
+		$this->setElementValue('StatusDetail', 'checked');
+		
+		$this->create_AccountInfo($accountinfo);
+	}
+	
+	public function create_AccountInfo($accountinfo)
+	{
+		$this->AccountInfo = self::createElement('AccountInfo');
+		$this->Response->appendChild($this->AccountInfo);
+		
+		foreach($accountinfo as $parameter=>$value)
 		{
-			$this->AccountInfo = self::createElement('AccountInfo');
-			$this->Response->appendChild($this->AccountInfo);
-			
-			foreach($accountinfo as $parameter=>$value)
-			{
-				$this->AccountInfo->appendChild(self::createElement($parameter, $value));
-			}
+			$this->AccountInfo->appendChild(self::createElement($parameter, $value));
 		}
 	}
+}
--- class/EasyPay/Provider31/Response/ErrorInfo.php
+++ class/EasyPay/Provider31/Response/ErrorInfo.php
@@ -1,15 +1,15 @@
 <?php
 
-	namespace EasyPay\Provider31\Response;
-	
-	use EasyPay\Provider31 as Provider31;
-	
-	final class ErrorInfo extends Provider31\Response
-	{
-		function __construct($code, $message) {
-			parent::__construct();
-			
-			$this->setElementValue('StatusCode', $code);
-			$this->setElementValue('StatusDetail', $message);
-		}
+namespace EasyPay\Provider31\Response;
+
+use \EasyPay\Provider31\Response as Response;
+
+final class ErrorInfo extends Response
+{
+	function __construct($code, $message) {
+		parent::__construct();
+		
+		$this->setElementValue('StatusCode', $code);
+		$this->setElementValue('StatusDetail', $message);
 	}
+}
