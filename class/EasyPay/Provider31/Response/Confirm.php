69c80033c71a4980568b1e63b62cd66f15ea8480
dshovchko <d.shovchko@gmail.com>
2017-09-19 15:36:48 +0300

    add Response/Confirm class

---
+++ class/EasyPay/Provider31/Response/Confirm.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ *      Class for response to Confirm request
+ *
+ *      @package php_EasyPay
+ *      @version 1.0
+ *      @author Dmitry Shovchko <d.shovchko@gmail.com>
+ *
+ */
+
+namespace EasyPay\Provider31\Response;
+
+use \EasyPay\Provider31\Response as Response;
+
+final class Confirm extends Response
+{
+	/**
+         *      Confirm constructor
+         *
+         *      @param string $orderdate
+         */
+	function __construct($orderdate) {
+		parent::__construct();
+		
+		$this->setElementValue('StatusCode', 0);
+		$this->setElementValue('StatusDetail', 'checked');
+		
+		$this->create_OrderDate($orderdate);
+	}
+	
+	/**
+         *      Create OrderDate node
+         *
+         *      @param string $orderdate
+         */
+	public function create_OrderDate($orderdate)
+	{
+		$this->OrderDate = self::createElement('OrderDate', $orderdate);
+		$this->Response->appendChild($this->OrderDate);
+	}
+}
